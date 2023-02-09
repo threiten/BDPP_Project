a630834be39e24b4ec5559170d9099e2da13a84f
Thomas Reitenspiess <thomas.reitenspiess@ginkgo-analytics.com>
2023-02-09 11:37:18 +0100

    Running Training and dataset preparation based on huggingface transformers and datasets libraries

---
+++ prepareDataset.py
@@ -0,0 +1,47 @@
+import datasets
+import zarr
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import argparse
+
+def clean_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """
+    Reset index and remove leftover index column from dataframe
+    dataframe: pandas.DataFrame
+    """
+    dataframe = dataframe.reset_index()
+    del dataframe['index']
+
+    return dataframe
+
+def main(options):
+
+    dataset = zarr.load(options.dataFile)
+    inpDf = pd.DataFrame({'text': dataset['text'], 'party': dataset['party']})
+    trainDf, testDf = train_test_split(inpDf, test_size=0.25, shuffle=True)
+    trainDf, valDf = train_test_split(trainDf, test_size=0.1, shuffle=True)
+
+    trainDf = clean_dataframe(trainDf)
+    testDf = clean_dataframe(testDf)
+    valDf = clean_dataframe(valDf)
+
+    trainDs = datasets.Dataset.from_pandas(trainDf, split="train")
+    testDs = datasets.Dataset.from_pandas(testDf, split='test')
+    valDs = datasets.Dataset.from_pandas(valDf, split='validation')
+
+    dsDict = datasets.DatasetDict()
+
+    dsDict['train'] = trainDs
+    dsDict['test'] = testDs
+    dsDict['validation'] = valDs
+
+    dsDict.push_to_hub(f"{options.repoName}", token=options.hfToken)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    args = parser.add_argument_group()
+    args.add_argument('--dataFile', '-d', help="Path to the input dataset. Needs to be in zarr format.", action='store', type=str)
+    args.add_argument('--hfToken', '-t', help='Token for huggingface hub.', action='store', type=str)
+    args.add_argument('--repoName', '-n', help='Name for the dataset repository', action='store', type=str)
+    options = parser.parse_args()
+    main(options)
--- runTraining.py
+++ runTraining.py
@@ -1,135 +1,81 @@
-import torch
-import torch.nn as nn
-import torch.multiprocessing as mp
-import zarr
-import numpy as np
+import datasets
+import transformers
+import utils
 import argparse
-import os
-import pickle as pkl
-from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, TensorDataset
-from collections import Counter
-from string import punctuation
-from LSTMmodel import LSTMMultiClassWrapper
-from utils import party_dict, remove_comments, cut_text, pad_text
+import copy
+import torch
+import numpy as np
+import evaluate
 
-noWeight = [
-    'independent'
-]
+def tokenize_inputs(examples: datasets.Dataset, tokenizer: transformers.BasicTokenizer, truncation: bool=True):
+    tokenized_inputs = tokenizer(examples['text'], truncation=truncation)
+    labels = examples['party']
+    new_labels = []
+    for label in labels:
+        new_labels.append(utils.party_dict[label])
+    tokenized_inputs['labels'] = new_labels
 
-config_arr = [
-    # {'hiddenDim' : 256, 'nLayers': 2, 'lr': 0.001},
-    # # {'hiddenDim' : 256, 'nLayers': 2, 'lr': 0.01},
-    # # {'hiddenDim' : 512, 'nLayers': 2, 'lr': 0.001},
-    # {'hiddenDim' : 1024, 'nLayers': 2, 'lr': 0.001},
-    # {'hiddenDim' : 1024, 'nLayers': 2, 'lr': 0.0001}
-    # {'hiddenDim' : 1536, 'nLayers': 2, 'lr': 0.001}, #using too much mem
-    {'hiddenDim': 1024, 'nLayers': 2, 'lr': 0.0005}  # best so far
-    # {'hiddenDim' : 1024, 'nLayers': 2, 'lr': 0.0001}
-    # {'hiddenDim' : 1024, 'nLayers': 2, 'lr': 0.01}
-    # {'hiddenDim' : 256, 'nLayers': 4, 'lr': 0.001},
-    # {'hiddenDim' : 256, 'nLayers': 3, 'lr': 0.001},
-    # {'hiddenDim' : 256, 'nLayers': 2, 'lr': 0.001},
-    # {'hiddenDim' : 512, 'nLayers': 4, 'lr': 0.001},
-    # {'hiddenDim' : 384, 'nLayers': 5, 'lr': 0.001}
-]
+    return tokenized_inputs
 
+def compute_metrics(eval_preds):
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
 
-def main(options):
+    metric = evaluate.load('accuracy')
+    all_metrics = metric.compute(predictions=predictions, references=labels)
+    return {
+        "accuracy": all_metrics["accuracy"],
+    }
 
-    print('Loading Data!')
-    zarr_dir = zarr.load(options.inFile)
-    data_x = zarr_dir['text']
-    data_y = zarr_dir['party']
+def main(options: argparse.Namespace):
+    raw_datasets = datasets.load_dataset('threite/Bundestag-v2')
+    tokenizer = transformers.AutoTokenizer.from_pretrained('xlm-roberta-base')
 
-    print('Preparing Data!')
-    data_x = [ent.lower() for ent in data_x]
-    data_x = [remove_comments(ent, ['(', ')']) for ent in data_x]
-    data_x = [''.join([c for c in ent if ent not in punctuation])
-              for ent in data_x]
+    tokenized_datasets = raw_datasets.map(tokenize_inputs, batched=True, remove_columns=raw_datasets['train'].column_names, fn_kwargs={'tokenizer': tokenizer, 'truncation': True})
 
-    print('Getting encoding!')
-    words = ' '.join(data_x).split()
-    sorted_words = Counter(words).most_common(len(words))
-    vocab_to_int = {w: i+1 for i, (w, c) in enumerate(sorted_words)}
-    with open('{}/out/vocab.pkl'.format(options.tmpDir), 'wb') as f:
-        pkl.dump(vocab_to_int, f)
-        f.close()
+    id2label={v: k for k, v in utils.party_dict.items() if k not in ['', 'independent']}
+    id2label[6] = 'unknown'
+    label2id = copy.deepcopy(utils.party_dict)
+    label2id['unknown'] = 6
+    del label2id['independent']
+    del label2id['']
 
-    print('Getting encoded Data!')
-    text_int = []
-    for tt in data_x:
-        text_int.append([vocab_to_int[w] for w in tt.split()])
-    text_int = np.array(text_int, dtype=object)
+    model = transformers.AutoModelForSequenceClassification.from_pretrained('xlm-roberta-base', label2id=label2id, id2label=id2label)
+    data_collator = transformers.DataCollatorWithPadding(tokenizer=tokenizer, padding=True)
+    
+    use_mps = torch.backends.mps.is_available()
+    
+    push_to_hub = options.hfToken is not None
+    training_args = transformers.TrainingArguments(
+        options.modelName,
+        evaluation_strategy='steps',
+        save_strategy='epoch',
+        learning_rate=2e-5,
+        num_train_epochs=4,
+        weight_decay=0.01,
+        use_mps_device=use_mps,
+        push_to_hub=push_to_hub,
+        hub_token=options.hfToken,
+        eval_steps=5000
+    )
 
-    print('Making labels!')
-    enc_party_label = np.array(
-        [party_dict[lab] if lab in party_dict.keys() else -1 for lab in data_y])
-    text_int = text_int[enc_party_label != -1]
-    enc_party_label = enc_party_label[enc_party_label != -1]
+    trainer = transformers.Trainer(
+        model=model,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+        args=training_args,
+        train_dataset=tokenized_datasets['train'],
+        eval_dataset=tokenized_datasets['validation'],
+        compute_metrics=compute_metrics
+    )
 
-    print('Cutting and padding text')
-    cutTxt, cutLbl = cut_text(text_int, enc_party_label, 500)
-    paddedTxt = pad_text(cutTxt, 500)
+    trainer.train()
+    trainer.push_to_hub()
 
-    trainTxt, testTxt, trainLbl, testLbl = train_test_split(
-        paddedTxt, cutLbl, test_size=0.2)
-    trainTxt, valTxt, trainLbl, valLbl = train_test_split(
-        trainTxt, trainLbl, test_size=0.2)
-    counterLabels = Counter(trainLbl)
-    # weightsVec = np.array([1./float(counterLabels[i]) for i in range(len(party_dict.keys()))])
-    # weightsVec[weightsVec>1.5*np.median(weightsVec)] = 1.5 * np.median(weightsVec)
-    # weightsVec[weightsVec<0.5*np.median(weightsVec)] = 0.5 * np.median(weightsVec)
-    # weightsVec *= len(weightsVec)/float(weightsVec.sum())
-    # print('Weights: ', weightsVec)
-    # weightsTens = torch.from_numpy(weightsVec)
-
-    train_data = TensorDataset(torch.from_numpy(
-        trainTxt), torch.from_numpy(trainLbl))
-    valid_data = TensorDataset(torch.from_numpy(
-        valTxt), torch.from_numpy(valLbl))
-    test_data = TensorDataset(torch.from_numpy(
-        testTxt), torch.from_numpy(testLbl))
-
-    batch_size = 30
-    train_loader = DataLoader(train_data, shuffle=True,
-                              batch_size=batch_size, drop_last=True)
-    valid_loader = DataLoader(valid_data, shuffle=True,
-                              batch_size=batch_size, drop_last=True)
-    test_loader = DataLoader(test_data, shuffle=True,
-                             batch_size=batch_size, drop_last=True)
-
-    train_on_gpu = torch.cuda.is_available()
-    avail_gpus = options.cudaDevice.split(',') if ',' in options.cudaDevice else [
-        options.cudaDevice]
-    # .format(avail_gpus[0])
-    device = torch.device('cuda' if train_on_gpu else 'cpu')
-    print('Training on gpu') if train_on_gpu else print('Training on cpu')
-
-    print('Getting network!')
-    config = config_arr[options.config]
-    print('Using config: ', config)
-    net = LSTMMultiClassWrapper(len(vocab_to_int)+1, len(party_dict.keys()),
-                                400, config['hiddenDim'], config['nLayers'], device=device)
-
-    print('Starting to train!')
-    net.train(train_loader=train_loader, valid_loader=valid_loader,
-              lr=config['lr'], epochs=10, batch_size=batch_size)  # weights=weightsTens
-
-    net.save(options.saveFile)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--cudaDevice', '-c', action='store', default='0', type=str)
-    parser.add_argument(
-        '--inFile', '-i', action='store', type=str)
-    parser.add_argument(
-        '--tmpDir', '-t', action='store', default='./', type=str)
-    parser.add_argument(
-        '--saveFile', '-s', action='store', type=str)
-    parser.add_argument(
-        '--config', action='store', type=int)
+    args = parser.add_argument_group()
+    args.add_argument('--modelName', '-n', action='store', type=str, help='Name of the model used when pushed to the huggingface hub')
+    args.add_argument('--hfToken', '-t', action='store', type=str, help='Acces Token for the huggingface hub')
     options = parser.parse_args()
     main(options)
